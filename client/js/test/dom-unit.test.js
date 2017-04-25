39f9c02ae56c9054911af83b84a88c7671710437
Kai Mashima <kaiomashima@gmail.com>
2017-04-25 15:52:42 -0400

    DOM creation unit test

--- client/js/test/dom-unit.test.js
+++ client/js/test/dom-unit.test.js
@@ -1,20 +1,49 @@
-const { removeChild } = require('../dom-util');
+const { createTR,
+        createTH,
+        createTD,
+        removeChild } = require('../dom-util');
 
 describe('dom-util', () => {
-  it('remove one child', () => {
-    // set up initial state
-    const parent = document.createElement('DIV');
-    const child = document.createElement('STRONG');
-    parent.appendChild(child);
 
-    // inspect initial state
-    expect(parent.childNodes.length).toBe(1);
-    expect(parent.childNodes[0]).toBe(child);
+  describe('DOM creation functions', () => {
+    describe('createTH', () => {
+      it('produces valid TH element', () => {
+        const el = createTH();
+        expect(el.tagName).toBe('TH');
+      });
+    });
 
-    // execute code under test
-    removeChild(parent);
+    describe('createTR', () => {
+      it('produces valid TR element', () => {
+        const el = createTR();
+        expect(el.tagName).toBe('TR');
+      });
+    });
 
-    // inspect resulting state
-    expect(parent.childNodes.length).toBe(0);
+    describe('createTD', () => {
+      it('produces valid TD element', () => {
+        const el = createTD();
+        expect(el.tagName).toBe('TD');
+      });
+    });
   });
+
+  describe('removeChildren()', () => {
+    it('remove one child', () => {
+        // set up initial state
+        const parent = document.createElement('DIV');
+        const child = document.createElement('STRONG');
+        parent.appendChild(child);
+
+        // inspect initial state
+        expect(parent.childNodes.length).toBe(1);
+        expect(parent.childNodes[0]).toBe(child);
+
+        // execute code under test
+        removeChild(parent);
+
+        // inspect resulting state
+        expect(parent.childNodes.length).toBe(0);
+      });
+  }); 
 });

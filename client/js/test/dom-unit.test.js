33dedfd4cf1f8c83a25e75a5f9182bc14b8d0770
Kai Mashima <kaiomashima@gmail.com>
2017-04-25 15:33:38 -0400

    DOM removal unit test

---
+++ client/js/test/dom-unit.test.js
@@ -0,0 +1,20 @@
+const { removeChild } = require('../dom-util');
+
+describe('dom-util', () => {
+  it('remove one child', () => {
+    // set up initial state
+    const parent = document.createElement('DIV');
+    const child = document.createElement('STRONG');
+    parent.appendChild(child);
+
+    // inspect initial state
+    expect(parent.childNodes.length).toBe(1);
+    expect(parent.childNodes[0]).toBe(child);
+
+    // execute code under test
+    removeChild(parent);
+
+    // inspect resulting state
+    expect(parent.childNodes.length).toBe(0);
+  });
+});

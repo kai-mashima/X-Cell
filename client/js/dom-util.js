d25358b424eddfcf4ba663abc9515976e9a09303
Kai Mashima <kaiomashima@gmail.com>
2017-04-25 15:06:09 -0400

    dom-util added

---
+++ client/js/dom-util.js
@@ -0,0 +1,26 @@
+const removeChild = function(parentEl) {
+  while (parentEl.firstChild) {
+    parentEl.removeChild(parentEl.firstChild);
+  }
+};
+
+const createEl = function(tagName) {
+  return function(text) {
+    const el = document.createElement(tagName);
+    if (text) {
+      el.textContent = text;
+    }
+    return el;
+  };
+};
+
+const createTR = createEl('TR');
+const createTH = createEl('TH');
+const createTD = createEl('TD');
+
+module.exports = {
+  createTR: createTR,
+  createTD: createTD,
+  createTH: createTH,
+  removeChild: removeChild
+}

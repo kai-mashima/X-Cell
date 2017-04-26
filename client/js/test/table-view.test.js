6a912f737275f90bf3908d542233b07444d17330
Kai Mashima <kaiomashima@gmail.com>
2017-04-25 22:38:45 -0400

    render table rows unit test

--- client/js/test/table-view.test.js
+++ client/js/test/table-view.test.js
@@ -11,6 +11,33 @@ describe('table-view', () => {
     document.documentElement.innerHTML = html;
   });
 
+  describe('table body', () => {
+    it('has the right size', () => {
+      // set the initial state
+      const numCols = 6;
+      const numRows = 10;
+      const model = new TableModel(numCols, numRows);
+      const view = new TableView(model);
+      view.init();
+
+      // inspect the initial state
+      let ths = document.querySelectorAll('THEAD TH');
+      expect(ths.length).toEqual(numCols);
+    });
+
+    it('fills in values from model', () => {
+      // set the initial state
+      const model = new TableModel(3, 3);
+      const view = new TableView(model);
+      model.setValue({col: 2, row: 1}, '123');
+      view.init();
+
+      // inspect the initial state
+      let trs = document.querySelectorAll('TBODY TR');
+      expect(trs[1].cells[2].textContent).toBe('123');
+    });
+  });
+
   describe('table header', () => {
     it('has valid column header labels', () => {
       // set up initial state
@@ -27,7 +54,6 @@ describe('table-view', () => {
       let lableTexts = Array.from(ths).map(el => el.textContent);
       expect(lableTexts).toEqual(['A', 'B', 'C', 'D', 'E', 'F']);
     });
-
   });
 
 });

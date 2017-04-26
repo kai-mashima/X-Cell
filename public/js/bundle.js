bfef0a4bb57e6a11a377fbcee9a4c6ba03c698db
Kai Mashima <kaiomashima@gmail.com>
2017-04-25 22:27:29 -0400

    render data rows

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -73,7 +73,7 @@ class TableModel {
 module.exports = TableModel;
 },{}],5:[function(require,module,exports){
 const { getLetterRange} = require('./array-util');
-const { createTH, removeChildren} = require('./dom-util');
+const { createTH, createTR, createTD, removeChildren} = require('./dom-util');
 
 class TableView {
   constructor(model) {
@@ -87,10 +87,12 @@ class TableView {
 
   initDomReferences () {
     this.headerRowEl = document.querySelector('THEAD TR');
+    this.sheetBodyEl = document.querySelector('TBODY');
   }
 
   renderTable () {
     this.renderTableHeader();
+    this.renderTableBody();
   }
 
   renderTableHeader () {
@@ -98,6 +100,22 @@ class TableView {
     getLetterRange('A', this.model.numCols)
       .map(colLabel => createTH(colLabel))
       .forEach(th => this.headerRowEl.appendChild(th));
+  }
+
+  renderTableBody () {
+    const fragment = document.createDocumentFragment();
+    for (let row = 0; row < this.model.numRows; row++) {
+      const tr = createTR();
+      for (let col = 0; col < this.model.numCols; col++) {
+        const position = {col: col, row: row};
+        const value = this.model.getValue(position);
+        const td = createTD(value);
+        tr.appendChild(td);
+      }
+      fragment.appendChild(tr);
+    }
+    removeChildren(this.sheetBodyEl);
+    this.sheetBodyEl.appendChild(fragment);
   }
 } 
 
